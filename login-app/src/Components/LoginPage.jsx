5565e564416c65a1a26d2cc31426ff472385431a
Dhruv_sharma <yes.dhruv@gmail.com>
2021-06-26 18:41:52 +0530

    firebase

--- login-app/src/Components/LoginPage.jsx
+++ login-app/src/Components/LoginPage.jsx
@@ -1,31 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import firebaseAuth from '../config/firebase';
 
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [user, setUser] = useState(null);
 
+    const handleLogin = async () => {
+        try {
+
+            let response = await firebaseAuth.signInWithEmailAndPassword(
+                username,
+                password
+            );
+            if (response.user.uid) {
+
+                setUser(response.user.uid);
+                setMessage("");
+            }
+        } catch (err) {
+            setMessage(err.message);
+        }
+    }
+
+    const handleLogout = () => {
+        firebaseAuth.signOut();
+        setUser(null);
+    }
+
+
+    useEffect( () => {
+        firebaseAuth.onAuthStateChanged((user)=>{
+            if(user){
+                setUser(user.uid);
+            }
+        });
+    } , [] );
     return (
         <div>
-            <h1>FireBase Login</h1>
-            <div>
-                <div>
-                    UserName{" "}
-                    <input value={username}
-                        onChange={(e) => setUsername(e.target.value)}></input>
-                </div>
-                <div>
-                    Password{" "}
-                    <input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></input>
-                </div>
-                <button>Login</button>
-            </div>
+            {user ? <div>
+                <h1>Welcome to HomePage, id is {user}</h1>
+                <button onClick={handleLogout}>Logout</button>
+            </div> : (
+                <>
+                    <h1> FireBase Login</h1>
+                    <div>
+                        Username <input value={username} onChange={(e) => setUsername(e.target.value)}></input>
+                    </div>
+                    <div>
+                        password <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    </div>
+                    <button onClick={handleLogin}>LOGIN</button>
+                    <h2 style={{ color: "red" }}>{message}</h2>
 
+                </>
+            )
+            }
         </div>
     );
 }
+
 
 export default LoginPage;
--- login-app/src/config/firebase.js
+++ login-app/src/config/firebase.js
@@ -1,11 +1,15 @@
-import firebase from 'firebase';
+import firebase from "firebase";
+
 let firebaseConfig = {
     apiKey: "AIzaSyBNxnJ4_z5PMsQQko9VX4dd527uPl7eSPE",
     authDomain: "login-9cf70.firebaseapp.com",
     projectId: "login-9cf70",
     storageBucket: "login-9cf70.appspot.com",
     messagingSenderId: "359647159751",
-    appId: "1:359647159751:web:c32edb0675ad6f849c25e2",
-    measurementId: "G-4LJNJ8T56C"
+    appId: "1:359647159751:web:c32edb0675ad6f849c25e2"
+    // measurementId: "G-4LJNJ8T56C"
   };
-let firebaseApp = firebase.initalizeApp
+  let firebaseApp = firebase.initializeApp(firebaseConfig);
+  let firebaseAuth = firebaseApp.auth();
+  
+  export default firebaseAuth;
